303b1d6d5b13908e6c420161c22e5806e7bff8bb
t0mer <tomer.klein@gmail.com>
2019-08-31 16:20:10 +0300

    initial commit

---
+++ MqttMon/MqttMon.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MqttMon
+{
+    partial class MqttMon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MqttMon));
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.btnSaveLog = new System.Windows.Forms.Button();
+            this.LogFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.output = new Controls.BetterRichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(729, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Address:";
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(100, 7);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(132, 20);
+            this.address.TabIndex = 3;
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(310, 6);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(192, 20);
+            this.Username.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Username:";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(570, 6);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(153, 20);
+            this.Password.TabIndex = 9;
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(510, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password:";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(810, 4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 10;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(892, 4);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLog.TabIndex = 11;
+            this.btnClearLog.Text = "Clear Log";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+            // 
+            // btnSaveLog
+            // 
+            this.btnSaveLog.Location = new System.Drawing.Point(973, 4);
+            this.btnSaveLog.Name = "btnSaveLog";
+            this.btnSaveLog.Size = new System.Drawing.Size(112, 23);
+            this.btnSaveLog.TabIndex = 12;
+            this.btnSaveLog.Text = "Save Log to File";
+            this.btnSaveLog.UseVisualStyleBackColor = true;
+            this.btnSaveLog.Visible = false;
+            this.btnSaveLog.Click += new System.EventHandler(this.btnSaveLog_Click);
+            // 
+            // LogFileDialog
+            // 
+            this.LogFileDialog.DefaultExt = "txt";
+            this.LogFileDialog.FileName = "MqttMon";
+            this.LogFileDialog.Title = "MqttMon - Save Log to File";
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.Black;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.output.ForeColor = System.Drawing.Color.White;
+            this.output.Location = new System.Drawing.Point(0, 33);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(1114, 596);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
+            // 
+            // MqttMon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 630);
+            this.Controls.Add(this.btnSaveLog);
+            this.Controls.Add(this.btnClearLog);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.output);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MqttMon";
+            this.Text = "MqttMon - Monitor all your mqtt messages";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.BetterRichTextBox output;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.Button btnSaveLog;
+        private System.Windows.Forms.SaveFileDialog LogFileDialog;
+    }
+}
+
---
+++ MqttMon/MqttMon.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Text;
+using System.IO;
+
+
+namespace MqttMon
+{
+    public partial class MqttMon : Form
+    {
+        private MqttClient client;
+        public MqttMon()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                if (client == null)
+                {
+                    this.output.AppendLine(Color.Yellow, "Connecting To server \n");
+                    client = new MqttClient(this.address.Text);
+                    client.MqttMsgPublished += client_MqttMsgPublished;
+                    client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+                    client.ConnectionClosed += Client_ConnectionClosed;
+                    var resule = client.Connect(new Guid().ToString(), this.Username.Text, this.Password.Text);
+                    ushort msgId = client.Subscribe(new string[] { "#", "w" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+                    this.output.AppendLine(Color.Green, "Connected To server \n");
+                }
+            }
+            catch (Exception ex)
+            {
+                this.output.AppendLine(System.Drawing.Color.Red, ex.Message.ToString() + "\n");
+                this.output.AppendLine(System.Drawing.Color.Red, "unable to connect to the server, please check your connection settings \n");
+                client = null;
+
+            }
+        }
+
+        private void DisConnect()
+        {
+            try
+            {
+                if (client != null)
+                {
+                    if (client.IsConnected)
+                    {
+                        this.output.AppendLine(Color.Yellow, "Disonnecting from server \n");
+                        client.Unsubscribe(new string[] { "#", "w" });
+                        client.MqttMsgPublished -= client_MqttMsgPublished;
+                        client.MqttMsgPublishReceived -= Client_MqttMsgPublishReceived;
+                        client.ConnectionClosed -= Client_ConnectionClosed;
+                        client.Disconnect();
+                        this.output.AppendLine(Color.Yellow, "Disconnected from server \n");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.output.AppendLine(System.Drawing.Color.Red, ex.Message.ToString());
+            }
+        }
+
+
+
+        private void Client_ConnectionClosed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            this.output.AppendLine(Color.Green, "Topic Received: " + e.Topic + "\n");
+            this.output.AppendLine(Color.White, "Data Received: " + Encoding.Default.GetString(e.Message) + "\n");
+            this.output.AppendLine(Color.Green, "\n");
+        }
+
+        private void client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.address.Text.Trim())){
+                this.output.AppendLine(Color.Red, "Please enter Server Address \n");
+                this.address.Focus();
+                return;
+            }
+
+            //if(!ValidateIPv4(this.address.Text.Trim()))
+            //{
+            //    this.output.AppendLine(Color.Red, "Please enter a valid Server Address \n");
+            //    this.address.Focus();
+            //    return;
+            //}
+
+            if (string.IsNullOrEmpty(this.Username.Text.Trim())){
+                this.output.AppendLine(Color.Red, "Please enter Username \n");
+                this.Username.Focus();
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(this.Password.Text.Trim())){
+                this.output.AppendLine(Color.Red, "Please enter Password \n");
+                this.Password.Focus();
+                return;
+            }
+            this.Connect();
+            this.CheckState();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            this.DisConnect();
+            this.CheckState();
+        }
+
+        private void CheckState()
+        {
+
+            if (client.IsConnected)
+            {
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+
+            }
+            else
+            {
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                client = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DisConnect();
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            this.output.Clear();
+        }
+
+        private void btnSaveLog_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
